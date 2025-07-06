f06eafb6f5cded7935eb3756d871d813ace9ddb4
Jayirong <lemonsoft5555@gmail.com>
2025-07-06 19:21:17 -0400

    Initial commit: proyecto iniciado para gestionar las imagenes del proyecto DYC

---
+++ src/main/java/com/microservicio/ms_imagenes_dyc/MsImagenesDycApplication.java
@@ -0,0 +1,13 @@
+package com.microservicio.ms_imagenes_dyc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsImagenesDycApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsImagenesDycApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/microservicio/ms_imagenes_dyc/MsImagenesDycApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservicio.ms_imagenes_dyc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsImagenesDycApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
